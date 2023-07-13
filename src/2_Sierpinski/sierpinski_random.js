368856d7ead002ff3cae1dbe4fa0682083198056
Aiman <aimanizales@gmail.com>
2023-07-12 23:18:14 -0500

    Sierpinski random: replaced tabs with spaces

--- src/2_Sierpinski/sierpinski_random.js
+++ src/2_Sierpinski/sierpinski_random.js
@@ -1,103 +1,102 @@
 window.onload = function() {
-	let triangles = [],
-		offset = 100,
-		context2D;
+    let triangles = [],
+        offset = 100,
+        context2D;
 
-	init();
+    init();
 
-	function init() {
-		let i,
-			angle = -Math.PI / 2;
+    function init() {
+        let i,
+            angle = -Math.PI / 2;
 
-		chaos.init();
+        chaos.init();
 
-		document.body.addEventListener("keyup", event => {
-			switch(event.key) {
-				case ' ':
-					subdivide();
-					draw();
-					break;
+        document.body.addEventListener("keyup", (event) => {
+            switch(event.key) {
+                case ' ':
+                    subdivide();
+                    draw();
+                    break;
 
-				case 'p':
-					chaos.popImage();
-					break;
+                case 'p':
+                    chaos.popImage();
+                    break;
 
-				default: 
-					break;
-			}
-		});
+                default: 
+                    break;
+            }
+        });
 
-		triangles[0] = [];
+        triangles[0] = [];
 
-		for (i = 0; i < 3; i += 1) {
-			triangles[0].push({
-				x: chaos.width * .5 + Math.cos(angle) * chaos.height * .5,
-				y: chaos.height * .65 + Math.sin(angle) * chaos.height * .5
-			});
-			angle += Math.PI * 2 / 3;
-		}
+        for (i = 0; i < 3; i += 1) {
+            triangles[0].push({
+                x: chaos.width * .5 + Math.cos(angle) * chaos.height * .5,
+                y: chaos.height * .65 + Math.sin(angle) * chaos.height * .5
+            });
+            angle += Math.PI * 2 / 3;
+        }
 
-		draw();
-	}
+        draw();
+    }
 
-	function draw() {
-		let i;
-		
-		context2D = chaos.context;
+    function draw() {
+        let i;
+        
+        context2D = chaos.context;
 
-		chaos.clear();
+        chaos.clear();
 
-		context2D.fillStyle = 'rgba(255, 0, 0, 0.5)';
-		context2D.strokeStyle = 'black';
-		context2D.lineWidth = 0.5;
-		context2D.beginPath();
+        context2D.fillStyle = 'rgba(255, 0, 0, 0.5)';
+        context2D.strokeStyle = 'black';
+        context2D.lineWidth = 0.5;
+        context2D.beginPath();
 
-		for(i = 0; i < triangles.length; i += 1) {
-			drawTriangle(triangles[i]);
-		}
+        for(i = 0; i < triangles.length; i += 1) {
+            drawTriangle(triangles[i]);
+        }
 
-		// context2D.fill();
-		context2D.stroke();
-	}
+        // context2D.fill();
+        context2D.stroke();
+    }
 
-	function drawTriangle(triangle) {
-		context2D.moveTo(triangle[0].x, triangle[0].y);
-		context2D.lineTo(triangle[1].x, triangle[1].y);
-		context2D.lineTo(triangle[2].x, triangle[2].y);
-		context2D.lineTo(triangle[0].x, triangle[0].y);
-	}
+    function drawTriangle(triangle) {
+        context2D.moveTo(triangle[0].x, triangle[0].y);
+        context2D.lineTo(triangle[1].x, triangle[1].y);
+        context2D.lineTo(triangle[2].x, triangle[2].y);
+        context2D.lineTo(triangle[0].x, triangle[0].y);
+    }
 
-	function subdivide() {
-		let i,
-			triangle,
-			p0, p1, p2, p3, p4, p5;
+    function subdivide() {
+        let i,
+            triangle,
+            p0, p1, p2, p3, p4, p5;
 
-		for(i = triangles.length - 1; i >= 0; i -= 1) {
-			triangle = triangles[i];
-			
-			p0 = triangle[0];
-			p1 = triangle[1];
-			p2 = triangle[2];
-			p3 = {
-				x: (p0.x + p1.x) / 2 + Math.random() * offset * 2 - offset,
-				y: (p0.y + p1.y) / 2 + Math.random() * offset * 2 - offset
-			};
-			p4 = {
-				x: (p1.x + p2.x) / 2 + Math.random() * offset * 2 - offset,
-				y: (p1.y + p2.y) / 2 + Math.random() * offset * 2 - offset
-			};
-			p5 = {
-				x: (p2.x + p0.x) / 2 + Math.random() * offset * 2 - offset,
-				y: (p2.y + p0.y) / 2 + Math.random() * offset * 2 - offset
-			};
-			triangles.push([p0, p3, p5]);
-			triangles.push([p3, p1, p4]);
-			triangles.push([p5, p4, p2]);
+        for(i = triangles.length - 1; i >= 0; i -= 1) {
+            triangle = triangles[i];
+            
+            p0 = triangle[0];
+            p1 = triangle[1];
+            p2 = triangle[2];
+            p3 = {
+                x: (p0.x + p1.x) / 2 + Math.random() * offset * 2 - offset,
+                y: (p0.y + p1.y) / 2 + Math.random() * offset * 2 - offset
+            };
+            p4 = {
+                x: (p1.x + p2.x) / 2 + Math.random() * offset * 2 - offset,
+                y: (p1.y + p2.y) / 2 + Math.random() * offset * 2 - offset
+            };
+            p5 = {
+                x: (p2.x + p0.x) / 2 + Math.random() * offset * 2 - offset,
+                y: (p2.y + p0.y) / 2 + Math.random() * offset * 2 - offset
+            };
+            triangles.push([p0, p3, p5]);
+            triangles.push([p3, p1, p4]);
+            triangles.push([p5, p4, p2]);
 
-			triangles.splice(i, 1);
-		}
-		offset *= 0.5;
-		// console.log(offset);
-	}
-
+            triangles.splice(i, 1);
+        }
+        offset *= 0.5;
+        // console.log(offset);
+    }
 }
