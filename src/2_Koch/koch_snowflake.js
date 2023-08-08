5d33b54df6f7b5748c2901c0a079c6ca039e26fe
Aiman <aimanizales@gmail.com>
2023-08-07 22:34:18 -0500

    Koch: Snowflake Small updates

--- src/2_Koch/koch_snowflake.js
+++ src/2_Koch/koch_snowflake.js
@@ -1,24 +1,23 @@
 window.onload = function() {
-	var maxDepth = 0;
+	let maxDepth = 0;
+
+	const { sqrt, sin, cos, atan2, PI } = Math;
 
 	init();
 
 	function init() {
-
 		chaos.init();
-
 
 		draw();
 
-		document.body.addEventListener("keyup", function(event) {
-			console.log(event.keyCode);
-			switch(event.keyCode) {
-				case 32: // space
+		document.body.addEventListener('keyup', event => {
+			switch(event.key) {
+				case ' ':
 					maxDepth += 1;
 					draw();
 					break;
 
-				case 80: // p
+				case 'p':
 					chaos.popImage();
 					break;
 
@@ -26,24 +25,22 @@ window.onload = function() {
 					break;
 			}
 		});
-
-
 	}
 
 	function draw() {
-		var p0 = {
+		let p0 = {
 			x: chaos.width * 0.32,
 			y: chaos.height * 0.28
 		}
 
-		var p1 = {
+		let p1 = {
 			x: chaos.width * 0.68,
 			y: chaos.height * 0.28
 		}
 
-		var p2 = {
-			x: p1.x + Math.cos(Math.PI * 2 / 3) * (p1.x - p0.x),
-			y: p1.y + Math.sin(Math.PI * 2 / 3) * (p1.x - p0.x)
+		let p2 = {
+			x: p1.x + cos(PI * 2 / 3) * (p1.x - p0.x),
+			y: p1.y + sin(PI * 2 / 3) * (p1.x - p0.x)
 		}
 
 		chaos.clear();
@@ -55,25 +52,26 @@ window.onload = function() {
 	}
 
 	function koch(p0, p1, depth) {
-		var dx = p1.x - p0.x,
+		let dx = p1.x - p0.x,
 			dy = p1.y - p0.y,
-			dist = Math.sqrt(dx * dx + dy * dy),
+			dist = sqrt(dx * dx + dy * dy),
 			unit = dist / 3,
-			angle = Math.atan2(dy, dx),
+			angle = atan2(dy, dx),
 			pa, pb, pc;
 
 		pa = {
-			x: p0.x + Math.cos(angle) * unit,
-			y: p0.y + Math.sin(angle) * unit
+			x: p0.x + cos(angle) * unit,
+			y: p0.y + sin(angle) * unit
 		};
 		pb = {
-			x: pa.x + Math.cos(angle - Math.PI / 3) * unit,
-			y: pa.y + Math.sin(angle - Math.PI / 3) * unit
+			x: pa.x + cos(angle - PI / 3) * unit,
+			y: pa.y + sin(angle - PI / 3) * unit
 		}
 		pc = {
-			x: p0.x + Math.cos(angle) * unit * 2,
-			y: p0.y + Math.sin(angle) * unit * 2
+			x: p0.x + cos(angle) * unit * 2,
+			y: p0.y + sin(angle) * unit * 2
 		}
+
 		if(depth === 0) {
 			chaos.context.beginPath();
 			chaos.context.moveTo(p0.x, p0.y);
@@ -82,13 +80,13 @@ window.onload = function() {
 			chaos.context.lineTo(pc.x, pc.y);
 			chaos.context.lineTo(p1.x, p1.y);
 			chaos.context.stroke();
-		}
-		else {
-			koch(p0, pa, depth -1);
-			koch(pa, pb, depth -1);
-			koch(pb, pc, depth -1);
-			koch(pc, p1, depth -1);
+		} else {
+			let newDepth = depth - 1
+
+			koch(p0, pa, newDepth);
+			koch(pa, pb, newDepth);
+			koch(pb, pc, newDepth);
+			koch(pc, p1, newDepth);
 		}
 	}
-
 }
