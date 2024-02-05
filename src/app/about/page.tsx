c6f267788f8f6c3afeca0f11d7549081dcb4b404
Aiman <aimanizales@gmail.com>
2024-02-04 23:42:33 -0500

    Added copy for About

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,38 @@
 export default function Page() {
-    return <h1>About</h1>;
+    return (
+        <>
+            <h1>About</h1>
+            <p style={{ fontSize: '1.5rem' }}>
+                Hi! <br />I am <strong>Aiman</strong> Abdel Samad, frontend
+                developer from Bogotá (with egiptian roots).{' '}
+                <strong>Graphic Designer</strong> from the Universidad Nacional
+                de Colombia, and I currently study{' '}
+                <strong>Engineering Informatics</strong> at the Universidad
+                Santo Tomás.
+            </p>
+            <p>
+                I work as <strong>frontend developer</strong> full-time since
+                2013. Previously, graphic designer (
+                <strong>interactive and programmer</strong> in Flash for nine
+                years). I occasionally work in <strong>UX/UI</strong>,{' '}
+                <strong>branding</strong>, <strong>typographic</strong>,{' '}
+                <strong>editorial</strong> and{' '}
+                <strong>infographic design</strong>.
+            </p>
+            <p>
+                <strong>I love read</strong> (classics, sciences, sci-fi,
+                philosophy, art, history, biography... check out my{' '}
+                <a href="https://www.goodreads.com/dalaiman" target="_blank">
+                    Goodreads
+                </a>{' '}
+                page) and cats. I'm passionate about{' '}
+                <strong>mathematics</strong>, <strong>physics</strong>,{' '}
+                <strong>astronomy</strong>, <strong>cosmology</strong>,{' '}
+                <strong>watercolour painting</strong> and{' '}
+                <strong>classical piano</strong> (still learning the same
+                preludes from J.S. Bach since 2010).
+                {/* <!-- I'm new in <a href="https://dalaiman.wordpress.com/" target="_blank">writing</a> about anything. I'm open to suggestions. --> */}
+            </p>
+        </>
+    );
 }
