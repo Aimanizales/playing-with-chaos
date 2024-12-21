5a70b96f99298202ef175206e0e3eced66c63338
Aiman Ulloa <aiman_ulloa@apple.com>
2024-12-21 09:37:06 -0500

    Added SASS

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  sassOptions: {
+    additionalData: `$var: red;`,
+    implementation: 'sass-embedded',
+  },
+};
+
+export default nextConfig;
