ed05d83ddc31c633df107e13cafd25e4a8f51ba7
Aiman Ulloa <aiman_ulloa@apple.com>
2025-01-31 18:48:52 -0500

    Chore: nextconfig

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,17 @@ const nextConfig: NextConfig = {
     additionalData: `$var: red;`,
     implementation: 'sass-embedded',
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images-na.ssl-images-amazon.com',
+        port: '',
+        pathname: '/images/S/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
